99745edb71d09d82981c347b63ec58df149a1be4
cstuart1310 <cstuart1310@gmail.com>
2023-01-05 13:34:39 +0000

    Changed to auto-find root

--- RedirectChecker.py
+++ RedirectChecker.py
@@ -1,17 +1,19 @@
 from urllib import request
+from urllib import error
 from bs4 import BeautifulSoup
 
 import csv
+import os
 
-
-outFile=open("C:\\Users\\S20103502\\Documents\\Work\\Placement Year\\Redirects.csv","w",newline='')#output spreadsheet
+root=os.path.dirname(os.path.abspath(__file__))+"\\"
+outFile=open(root+"Redirects.csv","w",newline='')#output spreadsheet
 writer = csv.writer(outFile)#starts the writer
 header=["Course",	"URL",	"Added placement?",	"Mentions Placement Year in course spec?","Redirects to another page?",	"Redirect URL","Double Checked?", "Notes"]#headers to be written to a spreadsheet
 
 
 writer.writerow(header)#Writes the headers
 
-inpFile=open("C:\\Users\\S20103502\\Documents\\Work\\Placement Year\\input.txt","r")#input list of urls to test
+inpFile=open(root+"links.txt","r")#input list of urls to test
 links=inpFile.readlines()
 redirects=[]
 
@@ -26,7 +28,7 @@ for url in links:
         new_url = str(response.geturl())#gets new url
         soup = BeautifulSoup(request.urlopen(url),features="html.parser")#reads the html as soup
         title= (soup.title.string)#gets the page title from soup
-    except urllib.error.HTTPError:#Either caused by the url having no destination or network issues
+    except:#Either caused by the url having no destination or network issues
         print("404")
 
     if url!=new_url:#if the original url isn't the same as the destination one
