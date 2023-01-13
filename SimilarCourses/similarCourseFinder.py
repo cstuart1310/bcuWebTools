904d1080a4844b06da9cac9762864d0a11531f68
cstuart1310 <54192276+cstuart1310@users.noreply.github.com>
2023-01-13 16:04:28 +0000

    Outputs a string not a whole array
    basically just looks prettier

--- SimilarCourses/similarCourseFinder.py
+++ SimilarCourses/similarCourseFinder.py
@@ -60,7 +60,7 @@ def findUSPs(siteURL,site):#Returns a list of USPs for each site
                     USPLine=result.split("\n")[lineCount+1]#move to the next line which hopefully has the data
                     USPList[4]="Had to move to next line"
                 USPLine=USPLine.replace("</span>","")#Removes spans so that course names can consistently be regexed out even with inconsistent code
-                USPLine=re.findall(r'">([^<]+)</a>',USPLine)#Finds the course name (Filters for a-z after ">  )
+                USPLine=re.findall(r'">([^<]+)</a>',USPLine)[0]#Finds the course name (Filters for a-z after ">  )
                 USPList.append(USPLine)#Adds the line to a list of USPs for this page
             lineCount+=1
         writer.writerow(USPList)#Writes the USPs and the URL to a csv
