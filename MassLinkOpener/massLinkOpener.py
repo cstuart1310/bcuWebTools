96b502e1d98d0fc26e7f539213573ca281efdb5c
Callum Stuart <Callum.Stuart@mail.bcu.ac.uk>
2023-06-30 14:02:42 +0100

    Update massLinkOpener.py

--- MassLinkOpener/massLinkOpener.py
+++ MassLinkOpener/massLinkOpener.py
@@ -1,10 +1,14 @@
 import webbrowser
 import time
+import os
+root=os.path.dirname(os.path.abspath(__file__))+"//"
 
-URLfile=open("massURLs.txt","r")
+URLfile=open(root+"massURLs.txt","r")
 
 urls=URLfile.readlines()
-
+urlCounter=0
 for url in urls:
+    urlCounter+=1
     webbrowser.open(url)
-    time.sleep(0.2)
+    print(url)
+    time.sleep(0.5)
